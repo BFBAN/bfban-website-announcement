22399d946110bfd83171ddad3da9caac3b8a8013
Cabbagelol <nickmiku@foxmail.com>
2024-12-30 21:24:08 +0800

    v0.0.2+8

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,73 +26,85 @@ export default function Home(): React.ReactElement {
                 {/*Main Row S*/}
                 <div className="container">
                     <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-2 py-5">
-                        <div className="col">
-                            <div className="border rounded-4 feature bg-gradient p-3 card">
-                                <div
-                                    className="feature-icon d-inline-flex align-items-center justify-content-start fs-1 mb-3">
-                                    <i className="bi bi-book"></i>
-                                </div>
-                                <h3 className="fs-2 text-body-emphasis">
-                                    <Translate id="personnel.title"></Translate>
-                                </h3>
-                                <p>
+                        <div className="col h-100">
+                            <div className="rounded-4 feature bg-gradient card">
+                                <div className="card-body">
+                                    <div
+                                        className="feature-icon d-inline-flex align-items-center justify-content-start fs-1 mb-3">
+                                        <i className="bi bi-book"></i>
+                                    </div>
+                                    <h3 className="fs-2 text-body-emphasis">
+                                        <Translate id="personnel.title"></Translate>
+                                    </h3>
+                                    <p>
                                     <span className="badge rounded-pill text-bg-dark">
                                         <Translate id="home.firstEdition"></Translate>
                                     </span>
-                                </p>
-                                <p>
-                                    <Translate id="personnel.description"></Translate>
-                                </p>
-                                <Link href="/precepts/tags/personnel" className="icon-link">
-                                    <Translate id="home.refer"></Translate>
-                                </Link>
+                                    </p>
+                                    <p className="card-text">
+                                        <Translate id="personnel.description"></Translate>
+                                    </p>
+                                </div>
+                                <div className="card-footer">
+                                    <Link href="/precepts/tags/personnel" className="icon-link card-link">
+                                        <Translate id="home.refer"></Translate>
+                                        <i className="bi bi-arrow-right"></i>
+                                    </Link>
+                                </div>
                             </div>
                         </div>
                         <div className="col">
-                            <div className="border rounded-4 feature bg-gradient p-3 card">
-                                <div
-                                    className="feature-icon d-inline-flex align-items-center justify-content-start fs-1 mb-3">
-                                    <i className="bi bi-book"></i>
+                            <div className="rounded-4 feature bg-gradient card">
+                                <div className="card-body">
+                                    <div
+                                        className="feature-icon d-inline-flex align-items-center justify-content-start fs-1 mb-3">
+                                        <i className="bi bi-book"></i>
+                                    </div>
+                                    <h3 className="fs-2 text-body-emphasis">
+                                        <Translate id="precepts.title"></Translate>
+                                    </h3>
+                                    <p>
+                                        <span className="badge rounded-pill text-bg-danger"><Translate
+                                            id="home.updata"></Translate></span>
+                                        <span className="badge rounded-pill text-bg-dark">v4</span>
+                                    </p>
+                                    <p className="card-text">
+                                        <Translate id="precepts.description"></Translate>
+                                    </p>
                                 </div>
-                                <h3 className="fs-2 text-body-emphasis">
-                                    <Translate id="precepts.title"></Translate>
-                                </h3>
-                                <p>
-                                    <span className="badge rounded-pill text-bg-danger">
-                                        <Translate id="home.updata"></Translate>
-                                    </span>
-                                    <span className="badge rounded-pill text-bg-dark">
-                                        v4
-                                    </span>
-                                </p>
-                                <p>
-                                    <Translate id="precepts.description"></Translate>
-                                </p>
-                                <Link href="/precepts/tags/precepts" className="icon-link">
-                                    <Translate id="home.refer"></Translate>
-                                </Link>
+                                <div className="card-footer">
+                                    <Link href="/precepts/tags/precepts" className="icon-link card-link">
+                                        <Translate id="home.refer"></Translate>
+                                        <i className="bi bi-arrow-right"></i>
+                                    </Link>
+                                </div>
                             </div>
                         </div>
-                        <div className="col">
-                            <div className="border rounded-4 feature bg-gradient p-3 card">
-                                <div
-                                    className="feature-icon d-inline-flex align-items-center justify-content-start fs-1 mb-3">
-                                    <i className="bi bi-book"></i>
+                        <div className="col h-100">
+                            <div className="rounded-4 feature bg-gradient  card">
+                                <div className="card-body">
+                                    <div
+                                        className="feature-icon d-inline-flex align-items-center justify-content-start fs-1 mb-3">
+                                        <i className="bi bi-book"></i>
+                                    </div>
+                                    <h3 className="fs-2 text-body-emphasis">
+                                        <Translate id="conduct.title"></Translate>
+                                    </h3>
+                                    <p>
+                                        <span className="badge rounded-pill text-bg-dark">
+                                            <Translate id="home.firstEdition"></Translate>
+                                        </span>
+                                    </p>
+                                    <p className="card-text">
+                                        <Translate id="conduct.description"></Translate>
+                                    </p>
                                 </div>
-                                <h3 className="fs-2 text-body-emphasis">
-                                    <Translate id="conduct.title"></Translate>
-                                </h3>
-                                <p>
-                                    <span className="badge rounded-pill text-bg-dark">
-                                        <Translate id="home.firstEdition"></Translate>
-                                    </span>
-                                </p>
-                                <p>
-                                    <Translate id="conduct.description"></Translate>
-                                </p>
-                                <Link href="/precepts/tags/conduct" className="icon-link">
-                                    <Translate id="home.refer"></Translate>
-                                </Link>
+                                <div className="card-footer">
+                                    <Link href="/precepts/tags/conduct" className="icon-link card-link">
+                                        <Translate id="home.refer"></Translate>
+                                        <i className="bi bi-arrow-right"></i>
+                                    </Link>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -100,25 +112,51 @@ export default function Home(): React.ReactElement {
                 {/*Main Row E*/}
 
                 <div className="container mb-5">
-                    <div className="row row-cols-4 align-items-stretch">
-                        <div className="d-flex w-2">
-                            <div className="border rounded-4 feature bg-gradient p-3 card">
-                                <h3 className="fs-2 text-body-emphasis">
-                                    <Translate id="privacy-policy.title">隐私政策</Translate>
-                                </h3>
-                                <p>
-                                    <Translate id="privacy-policy.description">保护您的隐私和个人数据对我们非常重要，向公众展示我们对隐私内容</Translate>
-                                </p>
+                    <div className="row row-cols-3 align-items-stretch">
+                        <div className="d-flex card m-3">
+                            <div className="row g-0 h-100">
+                                <div
+                                    className="col-3 align-content-center justify-items-center text-center"
+                                    style={{backgroundColor: 'var(--ifm-color-primary)'}}>
+                                    <i className="bi bi-eyeglasses fs-1 text-white"></i>
+                                </div>
+                                <div className="col-md-8">
+                                    <div className="card-body">
+                                        <Link href="/precepts/tags/privacy-policy" target="_blank">
+                                            <h3 className="fs-2 text-body-emphasis">
+                                                <Translate id="privacy-policy.title"></Translate>
+                                            </h3>
+                                        </Link>
+                                        <p>
+                                            <span className="badge rounded-pill text-bg-danger"><Translate
+                                                id="home.updata"></Translate></span>
+                                        </p>
+                                        <Translate id="privacy-policy.description"></Translate>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div className="d-flex w-2">
-                            <div className="border rounded-4 feature bg-gradient p-3 card">
-                                <h3 className="fs-2 text-body-emphasis">
-                                    <Translate id="exterior-design.title">设计规范</Translate>
-                                </h3>
-                                <p>
-                                    <Translate id="exterior-design.description">公示我们对于图标用途，指导正确使用LOGO以及外链部件</Translate>
-                                </p>
+                        <div className="d-flex card m-3">
+                            <div className="row g-0 h-100">
+                            <div
+                                    className="col-3 align-content-center justify-items-center text-center"
+                                    style={{backgroundColor: 'var(--ifm-color-primary)'}}>
+                                    <i className="bi bi-palette d-block fs-1 text-white"></i>
+                                </div>
+                                <div className="col-8">
+                                    <div className="card-body">
+                                        <Link href="/precepts/tags/exterior-design" target="_blank">
+                                            <h3 className="fs-2 text-body-emphasis">
+                                                <Translate id="exterior-design.title"></Translate>
+                                            </h3>
+                                        </Link>
+                                        <p>
+                                            <span className="badge rounded-pill text-bg-danger"><Translate
+                                                id="home.updata"></Translate></span>
+                                        </p>
+                                        <Translate id="exterior-design.description"></Translate>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
