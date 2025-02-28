273587cab284fdcf19fe84a9dff29eae2c4dfb9a
Cabbagelol <nickmiku@foxmail.com>
2025-03-01 07:15:26 +0800

    v0.0.3+9

--- src/components/_Lantern/index.tsx
+++ src/components/_Lantern/index.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './index.css';
 
 export default function Index({}) {
+    const [nowTime] = useState<any>(new Date().getTime())
+
     return (
-        <div className="lantern-widget-box"
-             v-if="nowTime >= new Date('2024 12-31').getTime() && nowTime <= new Date('2025 2-4').getTime()">
-            <div className="lantern-box">
-                {['left', 'right'].map(i => (
-                    <div className={`lantern ${i}-lantern`} key={i}>
-                        <div className="lantern-rope lantern-top-rope "></div>
-                        <div className="lantern-handle lantern-top-handle"></div>
-                        <div className="lantern-center">新年</div>
-                        <div className="lantern-handle lantern-bottom-handle"></div>
-                        <div className="lantern-bottom">
-                            <div className="lantern-rope lantern-bottom-rope"></div>
-                            <div className="lantern-panicle"></div>
+        <>
+            {nowTime >= new Date('2024 12-31').getTime() && nowTime <= new Date('2025 2-4').getTime() ?
+                (
+                    <div className="lantern-widget-box">
+                        <div className="lantern-box">
+                            {['left', 'right'].map(i => (
+                                <div className={`lantern ${i}-lantern`} key={i}>
+                                    <div className="lantern-rope lantern-top-rope "></div>
+                                    <div className="lantern-handle lantern-top-handle"></div>
+                                    <div className="lantern-center">新年</div>
+                                    <div className="lantern-handle lantern-bottom-handle"></div>
+                                    <div className="lantern-bottom">
+                                        <div className="lantern-rope lantern-bottom-rope"></div>
+                                        <div className="lantern-panicle"></div>
+                                    </div>
+                                </div>
+                            ))}
                         </div>
                     </div>
-                ))}
-
-            </div>
-        </div>
-    );
+                ) : null
+            }
+        </>
+    )
+        ;
 }
